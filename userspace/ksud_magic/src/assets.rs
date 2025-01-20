090f4c0f02dcb63066b1f1e8c2783fc3baff6622
Rifat Azad <rifat.44.azad.rifs@gmail.com>
2025-01-20 13:31:18 +0600

    ksud: fixed "warning: elided lifetime has a name" and "warning: constant `SUSFSD_PATH` is never used"

--- userspace/ksud_magic/src/assets.rs
+++ userspace/ksud_magic/src/assets.rs
@@ -8,6 +8,8 @@ use crate::{defs::BINARY_DIR, utils};
 pub const RESETPROP_PATH: &str = concatcp!(BINARY_DIR, "resetprop");
 pub const BUSYBOX_PATH: &str = concatcp!(BINARY_DIR, "busybox");
 pub const BOOTCTL_PATH: &str = concatcp!(BINARY_DIR, "bootctl");
+
+#[allow(dead_code)]
 pub const SUSFSD_PATH: &str = concatcp!(BINARY_DIR, "susfsd");
 
 #[cfg(all(target_arch = "x86_64", target_os = "android"))]
